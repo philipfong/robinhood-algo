8b7b7a404ca1069860794ab121a0066ee058622e
Philip Fong <philip.fong@coenterprise.com>
2019-01-16 14:48:06 -0500

    Only output when update happens

--- trading.rb
+++ trading.rb
@@ -6,8 +6,15 @@ password = ENV['RH_PW']
 client_id = ENV['CID']
 
 @robinhood = Robinhood::REST::Client.new(email, password, client_id)
+res = @robinhood.quote_data('VTI')
+latest_timestamp = res["results"][0]["updated_at"]
+puts res["results"][0]
 
 while 1
-  res = @robinhood.quote_data('AAPL')
-  puts res["results"][0]["last_trade_price"]
+  res = @robinhood.quote_data('VTI')
+  new_timestamp = res["results"][0]["updated_at"]
+  if latest_timestamp != new_timestamp
+    latest_timestamp = new_timestamp
+    puts res["results"][0]
+  end
 end
